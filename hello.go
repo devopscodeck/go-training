e0ed62ffe6b5f37d3cc6c27dcb902b6bc96bd495
devopscodeck <you@example.com>
2022-05-04 20:20:34 -0300

    go for monitoring site

--- hello.go
+++ hello.go
@@ -2,44 +2,66 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
 )
 
 func main() {
-	var nome string = "Administrator"
+	exibeIntroducao()
+	for {
+		exibeMenu()
+		comando := leComando()
+
+		// Best pratic with switch for this case
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			exibindoLogs()
+		case 0:
+			os.Exit(0) //informa que saímos do código com sucesso
+		default:
+			fmt.Println("O comando digitado não corresponde as opções disponíveis.")
+			os.Exit(-1) //informa que algo saiu errado
+		}
+	}
+}
+
+func exibeIntroducao() {
+	nome := "Administrator"
 	var versao = 1.2
 
 	fmt.Println("Hello", nome)
 	fmt.Println("Este programa está na versão:", versao)
+}
 
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
+
+	return comandoLido
+}
+
+func exibeMenu() {
 	fmt.Println("1- Iniciar monitoramento")
 	fmt.Println("2- Exibir logs")
 	fmt.Println("0- Sair do Programa")
+}
 
-	var comando int
-	fmt.Scan(&comando)
-	fmt.Println("O comando escolhido foi", comando)
+func exibindoLogs() {
+	fmt.Println("Exibindo logs..")
+}
 
-	// Condictional convenctional with if and else:
-	// 	if comando == 1 {
-	// 		fmt.Println("Monitorando...")
-	// 	} else if comando == 2 {
-	// 		fmt.Printf("Exibindo logs..")
-	// 	} else if comando == 0 {
-	// 		fmt.Printf("Saindo do programa")
-	// 	} else {
-	// 		fmt.Printf("Comando não existe como opção")
-	// 	}
-	// }
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	site := "https://alura.com.br"
+	resp, _ := http.Get(site)
 
-	// Best pratic with switch for this case
-	switch comando {
-	case 1:
-		fmt.Println("Monitorando...")
-	case 2:
-		fmt.Println("Exibindo logs..")
-	case 0:
-		fmt.Println("Saindo do programa")
-	default:
-		fmt.Println("O comando digitado não corresponde as opções disponíveis.")
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "carregado com sucesso")
+	} else {
+		fmt.Println("Site:", site, "está com error: ", resp.StatusCode)
+
 	}
 }
