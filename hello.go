35136ebced16b4c141c8362f8725e42704d334fe
devopscodeck <you@example.com>
2022-05-04 22:35:20 -0300

    the final state

--- hello.go
+++ hello.go
@@ -4,8 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -26,6 +28,7 @@ func main() {
 			iniciarMonitoramento()
 		case 2:
 			exibindoLogs()
+			imprimeLogs()
 		case 0:
 			os.Exit(0) //informa que saímos do código com sucesso
 		default:
@@ -88,8 +91,10 @@ func testaSite(site string) {
 
 	if resp.StatusCode == 200 {
 		fmt.Println("Site:", site, "carregado com sucesso")
+		registraLog(site, true)
 	} else {
 		fmt.Println("Site:", site, "está com error: ", resp.StatusCode)
+		registraLog(site, false)
 	}
 
 }
@@ -116,4 +121,27 @@ func leSitesDoArquivo() []string {
 	}
 	arquivo.Close()
 	return sites
+}
+
+func registraLog(site string, status bool) {
+	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + site + "- online: " + strconv.FormatBool(status) + "\n")
+
+	arquivo.Close()
+
+}
+
+func imprimeLogs() {
+
+	arquivo, err := ioutil.ReadFile("log.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(arquivo))
 }
